d1ac295db513d4342abcdae7ca1e559d7a4eaf42
kingsawpdx <kingsaw@pdx.edu>
2025-01-14 16:31:23 -0800

    Completed fizzbuzz

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -9,6 +9,24 @@ Use console.log() to write the proper output to the command line.
 
 **/
 
+const fizzbuzz = () => {
+  for (let i = 1; i <= 100; i += 1) {
+    if (!(i % 3)) {
+      if (!(i % 5)) {
+        console.log("fizzbuzz");
+      } else {
+        console.log("fizz");
+      }
+    } else if (!(i % 5)) {
+      console.log("buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+fizzbuzz();
+
 // 1
 // 2
 // fizz
